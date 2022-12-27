3ce59d6a323916f650265ba22ca312a25677d00e
Brian <dpi.studio@gmail.com>
2022-12-28 01:21:11 +0800

    Add project files.

---
+++ demoWinForm6/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace demoWinForm6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Log";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "some text";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Show Log";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Tomato;
+            this.progressBar1.ForeColor = System.Drawing.Color.Tomato;
+            this.progressBar1.Location = new System.Drawing.Point(12, 86);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(377, 10);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Go Async";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(174, 102);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Go Async 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(265, 102);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Go Async 3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private ProgressBar progressBar1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ demoWinForm6/Form1.cs
@@ -0,0 +1,166 @@
+using System.Diagnostics;
+
+namespace demoWinForm6
+{
+    public partial class Form1 : Form
+    {
+        Form2 f;
+        Random r = new Random();
+        int itemCouns = 0;
+        int pInit = 0;
+        int pStep = 0;
+        int sleep = 2000;
+        public Form1()
+        {
+            InitializeComponent();
+
+            f = new Form2();
+            
+
+            itemCouns = r.Next(3, 6);
+            AddLog($"Test Round: {itemCouns}");
+            pInit = 100 % itemCouns;
+            pStep = 100 / itemCouns;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2_Click(this, new EventArgs());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddLog(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            f.Visible = (f.Visible) ? false : true;
+
+            if (f.Visible)
+            {
+                f.Location = new Point(this.Location.X + this.Width, this.Location.Y);
+                f.Height = this.Height;
+            }
+        }
+
+        private void AddLog(object log)
+        {
+            f.textBox1.AppendText( $"{log}{Environment.NewLine}");
+        }
+
+
+
+        private void StartProgress()
+        {
+            progressBar1.Value = pInit;
+            for (int i = 0; i < itemCouns; i++)
+            {
+                var watch = Stopwatch.StartNew();
+                doSomething();
+                AddLog($"{i} {watch.Elapsed}");
+                progressBar1.Value += pStep;
+            }
+        }
+        private async Task StartProgressAsync()
+        {
+            progressBar1.Value = pInit;
+            for (int i = 0; i < itemCouns; i++)
+            {
+                var watch = Stopwatch.StartNew();
+                await doSomethingAsync();
+                AddLog($"{i} {watch.Elapsed}");
+                progressBar1.Value += pStep;
+            }
+        }
+
+        private async Task StartProgress2Async()
+        {
+            List<Task<string>> Tasks = new List<Task<string>>();
+
+            progressBar1.Value = pInit;
+            for (int i = 0; i < itemCouns; i++)
+            {
+                var watch = Stopwatch.StartNew();
+                Tasks.Add(Task.Run(() => doSomething2Async(i)));
+                AddLog($"{i} {watch.Elapsed}");
+                progressBar1.Value += pStep;
+            }
+
+            await Task.WhenAll( Tasks);
+            
+        }
+
+        private async Task StartProgress3Async()
+        {
+            List<Task<string>> Tasks = new List<Task<string>>();
+
+            progressBar1.Value = pInit;
+            for (int i = 0; i < itemCouns; i++)
+            {
+                Tasks.Add(doSomething2Async(i));
+                progressBar1.Value += pStep;
+            }
+
+            var result = await Task.WhenAll(Tasks);
+            foreach (string s in result)
+            {
+                AddLog(s);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var watch = Stopwatch.StartNew();
+            StartProgress();
+            AddLog($"Sync Finished: {watch.Elapsed}");
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            var watch = Stopwatch.StartNew();
+            await StartProgressAsync();
+            AddLog($"Async Finished: {watch.Elapsed}");
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            var watch = Stopwatch.StartNew();
+            await StartProgress2Async();
+            AddLog($"Async 2 Finished: {watch.Elapsed}");
+        }
+
+        private void doSomething()
+        {
+            Thread.Sleep(sleep);
+        }
+
+        private async Task doSomethingAsync()
+        {
+            await Task.Delay(sleep);
+        }
+
+        private async Task<string> doSomething2Async(int i)
+        {
+            var watch = Stopwatch.StartNew();
+            await Task.Delay(sleep);
+            return $"{i} {watch.Elapsed}";
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e)
+        {
+            if (f.Visible)
+            {
+                f.Location = new Point(this.Location.X + this.Width, this.Location.Y);
+                f.Height = this.Height;
+            }
+        }
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            var watch = Stopwatch.StartNew();
+            await StartProgress3Async();
+            AddLog($"Async 3 Finished: {watch.Elapsed}");
+        }
+    }
+}
